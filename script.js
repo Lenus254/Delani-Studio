f8b6989fe3d951b4dcdd195541c0598efafa88b4
lenus254 <linus.cheruiyot@student.moringaschool.com>
2021-11-22 06:38:27 +0300

    created form function for user alert

--- script.js
+++ script.js
@@ -94,3 +94,19 @@ $(document).ready(function(){
       $("#portfolio8").hide();
     });
   });
+
+  $(document).ready(function(){
+    $("form").submit(function(event){
+      var name = $("input#NAME").val();
+      var email = $("input#EMAIL").val();
+      var message = $("textarea#MESSAGE").val();
+      if ($("input#NAME").val() && $("input#EMAIL").val()){
+        alert (name + ", we have received your message. Thank you for reaching out to us.");
+      }
+      else {
+        alert("Please enter your name and email!");
+      }
+      
+    });
+  
+  });
