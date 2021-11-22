42666fde0b7ace0615fcab667a62a90c3612d7d9
lenus254 <linus.cheruiyot@student.moringaschool.com>
2021-11-22 06:34:53 +0300

    animated development and product section

--- script.js
+++ script.js
@@ -8,3 +8,25 @@ $(document).ready(function(){
       $("#designpic").slideDown('2000');
     });
   });
+
+  $(document).ready(function(){
+    $("#developmentpic").click(function(){
+      $("#developmentpic").slideDown('2000').hide('1000');
+      $("#developmenttxt").show('2000');
+    });
+    $("#developmenttxt").click(function(){
+      $("#developmenttxt").slideUp('2000');
+      $("#developmentpic").slideDown('2000');
+    });
+  });
+
+  $(document).ready(function(){
+    $("#productpic").click(function(){
+      $("#productpic").slideDown('2000').hide('1000');
+      $("#producttxt").show('2000');
+    });
+    $("#producttxt").click(function(){
+      $("#producttxt").slideUp('2000');
+      $("#productpic").slideDown('2000');
+    });
+  });
